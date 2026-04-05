d6dd2d5da5eb3b88bb3fad75be30f1ad9499bb8c
Kunal Chaudhary <kunalchaudhary843305@gmail.com>
2026-04-05 22:54:02 +0530

    Problem - 1

--- Two_sum.java
+++ Two_sum.java
@@ -8,7 +8,7 @@ class Solution {
                 if(nums[i]+nums[j]== target)
                 {
                 int a[] = {i,j};
-                return a;
+                return a ;
                 }
 
             }

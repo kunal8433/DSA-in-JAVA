06397a9997ae1e7f792d93a5b760b40f3ac782b4
Kunal Chaudhary <kunalchaudhary843305@gmail.com>
2026-06-14 12:24:17 +0530

    Add files via upload

---
+++ problem_441.java
@@ -0,0 +1,28 @@
+public class problem_441 {
+    class Solution {
+    public int arrangeCoins(int n) {
+        int left = 0;
+        int right = n;
+    
+        while(left<=right){
+            int mid = left+(right-left)/2;
+            long coins = (long) mid * (mid + 1) / 2;
+
+            if(coins ==n){
+            
+                return mid;
+
+            }
+            else if (coins<n){
+                left = mid+1;
+            }
+            else {
+                right = mid-1;
+            }
+        }
+        return right;
+        
+    }
+}
+    
+}

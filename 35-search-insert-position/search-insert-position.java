412a552ade276e727b7e74f08c508a105fc82df8
Kunal Chaudhary <kunalchaudhary843305@gmail.com>
2026-06-27 20:21:42 +0530

    Time: 0 ms (100.00%) | Memory: 44.6 MB (65.53%) - LeetSync

--- 35-search-insert-position/search-insert-position.java
+++ 35-search-insert-position/search-insert-position.java
@@ -7,7 +7,7 @@ class Solution {
             if(nums[mid]==target){
                 return mid;
             }
-            else if(nums[mid]<target){
+            else if (nums[mid]<target){
                 left = mid+1;
             }
             else{

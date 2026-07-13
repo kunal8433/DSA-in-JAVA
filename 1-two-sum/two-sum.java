9a066cfe4f133ddf7956874742dae6f003a5ca26
Kunal Chaudhary <kunalchaudhary843305@gmail.com>
2026-07-13 22:36:01 +0530

    Time: 45 ms (26.01%) | Memory: 46.9 MB (73.30%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -3,14 +3,12 @@ class Solution {
         for(int i=0;i<nums.length;i++){
             for(int j=i+1;j<nums.length;j++){
                 if(nums[i]+nums[j]==target){
-                    int a[] = {i,j};
+                    int[] a = {i,j};
                     return a;
                 }
             }
         }
         return null;
-
-
         
     }
 }

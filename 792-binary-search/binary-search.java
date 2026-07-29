7b459dc151780d54c0c866cd56930e27b76f9e8d
Kunal Chaudhary <kunalchaudhary843305@gmail.com>
2026-07-29 17:51:14 +0530

    Time: 0 ms (100.00%) | Memory: 48.2 MB (69.60%) - LeetSync

--- 792-binary-search/binary-search.java
+++ 792-binary-search/binary-search.java
@@ -4,20 +4,17 @@ class Solution {
         int right = nums.length-1;
         while(left<=right){
             int mid = left+(right-left)/2;
+
             if(nums[mid]==target){
                 return mid;
             }
-            else if(nums[mid] <target){
-            left= mid+1;
-            }
-            else if(nums[mid]>target){
-                right = mid-1;
+            else if(nums[mid]<target){
+                left = mid+1;
             }
             else {
-                return -1;
+                right = mid-1;
             }
         }
         return -1;
-        
     }
 }

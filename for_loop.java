bbc3eca19fe25a27fc632bde123c75b3c2a95117
Kunal Chaudhary <kunalchaudhary843305@gmail.com>
2026-08-19 00:13:17 +0530

    Add files via upload

---
+++ for_loop.java
@@ -0,0 +1,10 @@
+public class for_loop {
+    public static void main(String[] args) {
+        for(int i = 1 ; i<=8 ; i++)
+        {
+            System.out.println("KUNAL"+i);
+           
+        }
+    }
+    
+}
